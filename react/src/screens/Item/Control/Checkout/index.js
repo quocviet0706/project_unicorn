35b6d8fffbc74d158c17f01c270d23f4845e390e
hieu294 <tranthehieu12>
2022-01-16 17:27:01 +0700

    update checkout

--- react/src/screens/Item/Control/Checkout/index.js
+++ react/src/screens/Item/Control/Checkout/index.js
@@ -2,11 +2,9 @@ import React, { useState } from "react";
 import cn from "classnames";
 import styles from "./Checkout.module.sass";
 import Icon from "../../../../components/Icon";
-// import LoaderCircle from "../../../../components/LoaderCircle";
-import Flow_Steps from "../Flow_Steps";
+import LoaderCircle from "../../../../components/LoaderCircle";
 import Modal from "../../../../components/Modal";
 import SuccessfullyPurchased from "../SuccessfullyPurchased";
-
 
 const items = [
   {
@@ -28,48 +26,86 @@ const items = [
 ];
 
 const Checkout = ({ className }) => {
-  const [visibleModalPurchase, setVisibleModalPurchase] = useState(false);
+  const [visibleFlowSteps, setvisibleFlowSteps] = useState(false);
+  const [visible_FlowSteps, setvisible_FlowSteps] = useState(false);
   return (
     <>
       <div className={cn(className, styles.checkout)}>
-        <div className={cn("h4", styles.title)}>Checkout</div>
-        <div className={styles.info}>
-          You are about to purchase <strong>C O I N Z</strong> from{" "}
-          <strong>UI8</strong>
-        </div>
-        <div className={styles.table}>
-          {items.map((x, index) => (
-            <div className={styles.row} key={index}>
-              <div className={styles.col}>{x.title}</div>
-              <div className={styles.col}>{x.value}</div>
+        <div className={styles.checkout_item}>
+          <div className={cn("h4", styles.title)}>Checkout</div>
+          <div className={styles.info}>
+            You are about to purchase <strong>C O I N Z</strong> from{" "}
+            <strong>UI8</strong>
+          </div>
+          <div className={styles.table}>
+            {items.map((x, index) => (
+              <div className={styles.row} key={index}>
+                <div className={styles.col}>{x.title}</div>
+                <div className={styles.col}>{x.value}</div>
+              </div>
+            ))}
+          </div>
+          <div className={styles.attention}>
+            <div className={styles.preview}>
+              <Icon name="info-circle" size="32" />
             </div>
-          ))}
-        </div>
-        <div className={styles.attention}>
-          <div className={styles.preview}>
-            <Icon name="info-circle" size="32" />
+            <div className={styles.details}>
+              <div className={styles.subtitle}>This creator is not verified</div>
+              <div className={styles.text}>Purchase this item at your own risk</div>
+            </div>
           </div>
-          <div className={styles.details}>
-            <div className={styles.subtitle}>This creator is not verified</div>
-            <div className={styles.text}>Purchase this item at your own risk</div>
+          <div className={styles.btns}>
+            <button className={cn("button", styles.button)}
+              onClick={() => setvisibleFlowSteps(!visibleFlowSteps)}
+            >
+              I understand, continue
+            </button>
+            <button className={cn("button-stroke", styles.button)}>Cancel</button>
           </div>
         </div>
-        <div className={styles.btns}>
-          <button className={cn("button", styles.button)}
-            className={cn("button", styles.button)}
-            onClick={() => setVisibleModalPurchase(true)}
+        <div className={cn(className, styles.checkout)}>
+          <div className={cn("h4", styles.box, { [styles.active]: visibleFlowSteps })}>
+            Follow steps
+            <div className={styles.line}>
+              <div className={styles.icon}>
+                <LoaderCircle className={styles.loader} />
+              </div>
+              <div className={styles.details}>
+                <div className={styles.subtitle}>Purchasing</div>
+                <div className={styles.text}>
+                  Sending transaction with your wallet
+                </div>
+              </div>
+            </div>
+            <div className={styles.attention}>
+              <div className={styles.preview}>
+                <Icon name="info-circle" size="32" />
+              </div>
+              <div className={styles.details}>
+                <div className={styles.subtitle}>This creator is not verified</div>
+                <div className={styles.text}>Purchase this item at your own risk</div>
+              </div>
+              <div className={styles.avatar}>
+                <img src="/images/discover/icon-avatar-discover.svg" alt="Avatar" />
+              </div>
+            </div>
+            <div className={styles.btns}>
+              <button className={cn("button", styles.button)}
+                onClick={() => setvisible_FlowSteps(true)}
+              >
+                I understand, continue
+              </button>
+              <button className={cn("button-stroke", styles.button)}>Cancel</button>
+            </div>
+          </div>
+          <Modal
+            visible={visible_FlowSteps}
+          // onClose={() => setvisible_FlowSteps(false)}
           >
-            I understand, continue
-          </button>
-          <button className={cn("button-stroke", styles.button)}>Cancel</button>
+            <SuccessfullyPurchased />
+          </Modal>
         </div>
       </div>
-      <Modal
-        visible={visibleModalPurchase}
-        onClose={() => setVisibleModalPurchase(false)}
-      >
-        <Flow_Steps />
-      </Modal>
     </>
   );
 };
