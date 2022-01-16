59cc446b7a7c69d29975a197299da11e8fbfd9e1
hieu294 <tranthehieu12>
2022-01-16 18:19:07 +0700

    update

--- react/src/screens/Item/Control/Checkout/index.js
+++ react/src/screens/Item/Control/Checkout/index.js
@@ -65,6 +65,7 @@ const Checkout = ({ className }) => {
         </div>
         <div className={cn(className, styles.checkout)}>
           <div className={cn("h4", styles.box, { [styles.active]: visibleFlowSteps })}>
+            <button class="Modal_close__2RPhD"><svg class="" width="14" height="14" viewBox="0 0 16 16" fill="inherit"><path d="M.335.335c.446-.446 1.17-.446 1.616 0L8 6.384 14.049.335c.446-.446 1.17-.446 1.616 0s.446 1.17 0 1.616L9.616 8l6.049 6.049c.446.446.446 1.17 0 1.616s-1.17.446-1.616 0L8 9.616l-6.049 6.049c-.446.446-1.17.446-1.616 0s-.446-1.17 0-1.616L6.384 8 .335 1.951c-.446-.446-.446-1.17 0-1.616"></path></svg></button>
             Follow steps
             <div className={styles.line}>
               <div className={styles.icon}>
