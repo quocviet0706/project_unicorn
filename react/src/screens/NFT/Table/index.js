a7a381225bf74cc0cbbe20575923bf575c178198
Võ Văn Trường <56033678+vovantruong@users.noreply.github.com>
2022-03-15 12:32:46 +0700

    commit

---
+++ react/src/screens/NFT/Table/index.js
@@ -0,0 +1,272 @@
+import React, { Component, useEffect } from "react";
+import { Header, Image, Icon, Table, Menu } from "semantic-ui-react";
+import { Link, NavLink } from "react-router-dom";
+import cn from "classnames";
+import styles from "./Table.module.sass";
+const style = (
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.1/dist/semantic.min.css"
+  />
+);
+
+const listHeader = [
+  "Collection",
+  " Volume",
+  "24h %",
+  "7h %",
+  "Floor Price",
+  "Onwers",
+  "Items",
+];
+
+const list = [
+  {
+    collection: [
+      {
+        img: "https://react.semantic-ui.com/images/avatar/small/lena.png",
+        name: "Lena",
+        sub: "Human Resources",
+      },
+    ],
+    volumn: "22.837,23",
+    twenty: "+312.22%",
+    seven: "-2.26%",
+    floor: "1.68",
+    onwers: "4.2K",
+    item: "5.0K",
+  },
+  {
+    collection: [
+      {
+        img: "https://react.semantic-ui.com/images/avatar/small/lena.png",
+        name: "Lena",
+        sub: "Human Resources",
+      },
+    ],
+    volumn: "22.837,23",
+    twenty: "+312.22%",
+    seven: "-2.26%",
+    floor: "1.68",
+    onwers: "4.2K",
+    item: "5.0K",
+  },
+  {
+    collection: [
+      {
+        img: "https://react.semantic-ui.com/images/avatar/small/lena.png",
+        name: "Lena",
+        sub: "Human Resources",
+      },
+    ],
+    volumn: "22.837,23",
+    twenty: "+312.22%",
+    seven: "-2.26%",
+    floor: "1.68",
+    onwers: "4.2K",
+    item: "5.0K",
+  },
+  {
+    collection: [
+      {
+        img: "https://react.semantic-ui.com/images/avatar/small/matthew.png",
+        name: "Matthew",
+        sub: "Fabric Design",
+      },
+    ],
+    volumn: "22.837,23",
+    twenty: "-2.22%",
+    seven: "+103.26%",
+    floor: "1.68",
+    onwers: "4.2K",
+    item: "5.0K",
+  },
+  {
+    collection: [
+      {
+        img: "https://react.semantic-ui.com/images/avatar/small/lindsay.png",
+        name: "Lindsay",
+        sub: "Fabric Design",
+      },
+    ],
+    volumn: "22.837,23",
+    twenty: "+212.22%",
+    seven: "-3.26%",
+    floor: "1.68",
+    onwers: "4.2K",
+    item: "5.0K",
+  },
+  {
+    collection: [
+      {
+        img: "https://react.semantic-ui.com/images/avatar/small/mark.png",
+        name: "Mark",
+        sub: "Fabric Design",
+      },
+    ],
+    volumn: "22.837,23",
+    twenty: "-9.22%",
+    seven: "+30.26%",
+    floor: "1.68",
+    onwers: "4.2K",
+    item: "5.0K",
+  },
+  {
+    collection: [
+      {
+        img: "https://react.semantic-ui.com/images/avatar/small/mark.png",
+        name: "Mark",
+        sub: "Fabric Design",
+      },
+    ],
+    volumn: "22.837,23",
+    twenty: "-9.22%",
+    seven: "+30.26%",
+    floor: "1.68",
+    onwers: "4.2K",
+    item: "5.0K",
+  },
+  {
+    collection: [
+      {
+        img: "https://react.semantic-ui.com/images/avatar/small/mark.png",
+        name: "Mark",
+        sub: "Fabric Design",
+      },
+    ],
+    volumn: "22.837,23",
+    twenty: "-9.22%",
+    seven: "+30.26%",
+    floor: "1.68",
+    onwers: "4.2K",
+    item: "5.0K",
+  },
+  {
+    collection: [
+      {
+        img: "https://react.semantic-ui.com/images/avatar/small/mark.png",
+        name: "Mark",
+        sub: "Fabric Design",
+      },
+    ],
+    volumn: "22.837,23",
+    twenty: "-9.22%",
+    seven: "+30.26%",
+    floor: "1.68",
+    onwers: "4.2K",
+    item: "5.0K",
+  },
+  {
+    collection: [
+      {
+        img: "https://react.semantic-ui.com/images/avatar/small/mark.png",
+        name: "Mark",
+        sub: "Fabric Design",
+      },
+    ],
+    volumn: "22.837,23",
+    twenty: "-9.22%",
+    seven: "+30.26%",
+    floor: "1.68",
+    onwers: "4.2K",
+    item: "5.0K",
+  },
+
+];
+
+const Tables = ({ className}) => {
+  useEffect(() => {
+    let up__down = document.querySelectorAll(".up__down");
+    for (let i = 0; i < up__down.length; i++) {
+      let str = up__down[i].innerText.slice(0, 1);
+      if (str == "+") {
+        up__down[i].style.setProperty("color", "#08E600", "important");
+      } else {
+        up__down[i].style.setProperty("color", "red", "important");
+      }
+    }
+
+    const bang = document.querySelectorAll(".ui.teal tbody tr");
+    bang.forEach((e) => {
+      e.addEventListener("click", () => {
+        window.location.href = "/activity";
+      });
+    });
+  });
+
+  return (
+    <div className={className}>
+      {style}
+      <Table
+        selectable
+        color="teal"
+        padded="very"
+        // striped="true"
+        celled
+        className={cn(styles.body)}
+        style={{ marginTop: "50px" }}
+      >
+        <Table.Header>
+          <Table.Row className={styles.row__header}>
+            {listHeader.map((header, index) => (
+              <Table.HeaderCell key={index}>{header}</Table.HeaderCell>
+            ))}
+          </Table.Row>
+        </Table.Header>
+        <Table.Body className={styles.table__body}>
+          {list.map((e, index) => (
+            <Table.Row key={index}>
+              <Table.Cell>
+                {e.collection.map((i, number) => (
+                  <Header key={number} as="h4" image>
+                    <Image src={i.img} rounded size="mini" />
+
+                    <Header.Content>
+                      {i.name}
+                      <Header.Subheader className={styles.sub}>
+                        {i.sub}
+                      </Header.Subheader>
+                    </Header.Content>
+                  </Header>
+                ))}
+              </Table.Cell>
+              <Table.Cell>{e.volumn}</Table.Cell>
+              <Table.Cell className={cn("up__down")}>{e.twenty}</Table.Cell>
+              <Table.Cell className={cn("up__down")}>{e.seven}</Table.Cell>
+              <Table.Cell>{e.floor}</Table.Cell>
+              <Table.Cell>{e.onwers}</Table.Cell>
+              <Table.Cell>{e.item}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+        <Table.Footer>
+          <Table.Row>
+            <Table.HeaderCell colSpan="7">
+              <Menu floated="right" pagination>
+                <Menu.Item as="a" icon>
+                  <Icon name="chevron left" />
+                </Menu.Item>
+                <Menu.Item as="a">1</Menu.Item>
+                <Menu.Item as="a">2</Menu.Item>
+                <Menu.Item as="a">3</Menu.Item>
+                <Menu.Item as="a">4</Menu.Item>
+                <Menu.Item as="a" icon>
+                  <Icon name="chevron right" />
+                </Menu.Item>
+              </Menu>
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>
+    </div>
+  );
+};
+
+export default Tables;
+
+// export default () => (
+//   <>
+//     {style}
+//     <Tables />
+//   </>
+// );
---
+++ react/src/screens/SellNFT/index.js
@@ -0,0 +1,267 @@
+import React, { useState } from "react";
+import cn from "classnames";
+import styles from "./SellNFT.module.sass";
+import Icon from "../../components/Icon";
+import Loader from "../../components/Loader";
+import Preview from "../UploadDetails/Preview";
+import Cards from "../UploadDetails/Cards";
+import Switch from "../../components/Switch";
+import TextInput from "../../components/TextInput";
+import Dropdown from "../../components/Dropdown";
+import { Link } from "react-router-dom";
+import Duration from "./Duration";
+
+const royaltiesOptions = [
+  "Sell to highest bidder",
+  "Sell with declining price",
+];
+const priceOptions = ["BNB", "ETH", "Polygon"];
+const items = [
+  {
+    title: "Create collection",
+    color: "#4BC9F0",
+  },
+  {
+    title: "Crypto Legend - Professor",
+    color: "#45B26B",
+  },
+  {
+    title: "Crypto Legend - Professor",
+    color: "#EF466F",
+  },
+  {
+    title: "Legend Photography",
+    color: "#9757D7",
+  },
+];
+
+const biddersOptions = [
+  "5 bidders",
+  "10 bidders",
+  "20 bidders",
+  "30 bidders",
+  "50 bidders",
+  "More options",
+];
+
+const SellNFT = () => {
+  const [royalties, setRoyalties] = useState(royaltiesOptions[0]);
+  const [price, setPrice] = useState(priceOptions[0]);
+  const [visibleModal, setVisibleModal] = useState(false);
+  const [active, setActive] = useState(true);
+  const [visiblePreview, setVisiblePreview] = useState(false);
+  const [locking, setLocking] = useState(false);
+  const [sale, setSale] = useState(true);
+  const [bidders, setBidders] = useState(biddersOptions[0]);
+
+  const handleClick = (e, value) => {
+    let newArr = value + " bidders";
+    if (e.key == "Enter") {
+      if (value != "") {
+        biddersOptions.push(newArr);
+        biddersOptions.sort()
+        setBidders(newArr);
+        e.target.value = "";
+      }
+    }
+  };
+
+  return (
+    <div className={cn("section", styles.section)}>
+      <div className={cn("container", styles.container)}>
+        <div className={styles.wrapper}>
+          <div className={styles.head}>
+            <div className={cn("h2", styles.title)}>List item for sale</div>
+          </div>
+          <div className={styles.icon_method}>
+            <div className={styles.label}>Type</div>
+            <div className={styles.iconWaring}>
+              <Icon name="info-circle" size="22" />
+              <div className={styles.message__warning}>
+                <p>
+                  Learn more about the two types of listing options in our{" "}
+                  <Link to="sell_nft">Help Center</Link>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.btnSellNFT}>
+            <button
+              style={
+                active
+                  ? {
+                      color: "#fff",
+                      border: "2px solid #a92e71",
+                      background: "#a92e71",
+                    }
+                  : null
+              }
+              className={styles.button}
+              type="button"
+              onClick={() => (active ? null : setActive(!active))}
+            >
+              <div className={styles.icon_time}>
+                <i className="fas fa-hourglass-half"></i>
+              </div>
+              Timed auctions
+            </button>
+            <button
+              style={
+                active
+                  ? null
+                  : {
+                      color: "#fff",
+                      border: "2px solid #a92e71",
+                      background: "#a92e71",
+                    }
+              }
+              className={styles.button}
+              type="button"
+              onClick={() => (active ? setActive(!active) : null)}
+            >
+              <div className={styles.advanced}>
+                <i className="fas fa-person-sign"></i>
+              </div>
+              Advanced auctions
+            </button>
+          </div>
+          <form className={styles.form} action="">
+            <div className={styles.list}>
+              <div className={styles.item}>
+                <div className={styles.fieldset}>
+                  <div className={styles.row}>
+                    <div className={styles.col}>
+                      <div className={styles.field}>
+                        <div className={styles.icon_method}>
+                          <div className={styles.label}>Method</div>
+                          <div className={styles.iconWaring}>
+                            <Icon name="info-circle" size="22" />
+                            <div
+                              className={cn(
+                                styles.message__warning,
+                                styles.msg__large
+                              )}
+                            >
+                              <p>
+                                List price and listing schedule cannot be edited
+                                once the item is listed. You will need to cancel
+                                your listing and relist the item with the
+                                updated price and dates.
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                        <Dropdown
+                          className={styles.dropdown}
+                          value={royalties}
+                          setValue={setRoyalties}
+                          options={royaltiesOptions}
+                        />
+                        <div className={styles.label}>Starting price</div>
+                        <div className={styles.starting_price}>
+                          <Dropdown
+                            className={styles.dropdown}
+                            value={price}
+                            setValue={setPrice}
+                            options={priceOptions}
+                          />
+                          <TextInput
+                            className={styles.field}
+                            placeholder="Amount"
+                            type="text"
+                          />
+                        </div>
+                        <div className={styles.label}>Duration</div>
+                        <div className={styles.Duration}>
+                          <button className={styles.btnDuration} type="button">
+                            <div className={styles.textDuration}>
+                              <i className="fas fa-calendar"></i>
+                              <span className={styles.spDuration}>
+                                Duration
+                              </span>
+                            </div>
+                          </button>
+                          <div className={styles.call__duration}>
+                            <Duration />
+                          </div>
+                        </div>
+                        {active ? null : (
+                          <>
+                            <div
+                              style={{ marginTop: 30 }}
+                              className={styles.label}
+                            >
+                              Total Bidders
+                            </div>
+                            <div className={styles.more__options}>
+                              <div className={styles.dropdown__bidders}>
+                                <Dropdown
+                                  value={bidders}
+                                  setValue={setBidders}
+                                  options={biddersOptions}
+                                />
+                              </div>
+                              {bidders ==
+                              biddersOptions[biddersOptions.length - 1] ? (
+                                <TextInput
+                                  className={styles.field__bidders}
+                                  placeholder="Enter total bidders ..."
+                                  type="number"
+                                  onKeyPress={(e) =>
+                                    handleClick(e, e.target.value)
+                                  }
+                                />
+                              ) : null}
+                            </div>
+                          </>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.fees_icon}>
+              <div className={styles.text}>Fees</div>
+              <div className={styles.iconWaring}>
+                <Icon name="info-circle" size="22" />
+                <div className={styles.message__warning}>
+                  <p>
+                    Listing is free. Once sold, the following fees will be
+                    deducted.
+                    <Link to="sell_nft">Learn more</Link>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.Service}>
+              <div className={styles.text}>Service Fee</div>
+              <div className={styles.percent}>2.5%</div>
+            </div>
+            <div className={styles.foot}>
+              <button
+                className={cn("button", styles.button)}
+                onClick={() => setVisibleModal(true)}
+                // type="button" hide after form customization
+                type="button"
+              >
+                <span>Complete Listing</span>
+                <Icon name="arrow-next" size="10" />
+              </button>
+              <div className={styles.saving}>
+                <span>Auto saving</span>
+                <Loader className={styles.loader} />
+              </div>
+            </div>
+          </form>
+        </div>
+        <Preview
+          className={cn(styles.preview, { [styles.active]: visiblePreview })}
+          onClose={() => setVisiblePreview(false)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SellNFT;
